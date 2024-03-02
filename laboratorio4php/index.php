f99e643f4b76885e569ea9651d50d90220fe36c0
marcoz29 <108309661+marcoz29@users.noreply.github.com>
2024-03-01 19:31:44 -0600

    Add files via upload

---
+++ laboratorio4php/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$variable = 'Hola mundo';
+
+$variable = 54;
+
+$vector[0] = 52;
+$vector[1] = 40;
+$vector[] = 31;
+
+$lugares['playa'] ='Playa Jaco';
+$lugares['catarata'] = 'Catarata de la paz';
+$lugares['rio'] = 'Rio Tarcoles';
+
+print_r($vector);
+echo "<br>";
+echo "<br>";
+
+echo $variable ."caramelos";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
